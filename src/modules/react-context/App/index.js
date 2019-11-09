d4e608ce2463584eef817248f2ed74f6bbb48853
PavlyukVadim <pavlyuk.dev@gmail.com>
2019-11-09 15:52:23 +0200

    initial destructure

---
+++ src/modules/react-context/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  )
+}
